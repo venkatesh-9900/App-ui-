144958ba72f8c1188fdc4d2166e86b11b654b41b
pknxvii <preetamnahak.nitr@gmail.com>
2025-10-25 19:15:13 +0530

    miinor commit

--- next.config.ts
+++ next.config.ts
@@ -5,17 +5,17 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },  
-/* ENABLE FOR LOCAL DEVELOPMENT */
-  async rewrites() {
-    // Get API destination from environment variable, with fallback for development
-    const apiDestination = process.env.API_BASE_URL
-    return [
-      {
-        source: "/api/:path*",
-        destination: `${apiDestination}/api/:path*`,
-      },
-    ];
-  },
+// /* ENABLE FOR LOCAL DEVELOPMENT */
+//   async rewrites() {
+//     // Get API destination from environment variable, with fallback for development
+//     const apiDestination = process.env.API_BASE_URL
+//     return [
+//       {
+//         source: "/api/:path*",
+//         destination: `${apiDestination}/api/:path*`,
+//       },
+//     ];
+//   },
 };
 
 export default nextConfig;
