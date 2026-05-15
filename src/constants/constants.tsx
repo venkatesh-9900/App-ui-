8e634fbba64463c34fb9c0e76eb82259da39edb3
Apple <apple@Apples-MacBook-Pro.local>
2026-05-15 19:57:01 +0530

    Changed app_name and certificate details in helm value files of all environments

--- src/constants/constants.tsx
+++ src/constants/constants.tsx
@@ -1,4 +1,4 @@
-export const app_name = "argus-intelligence-web";
+export const app_name = "kernelmind-web";
 export const file_attachments_delimiter = `\n\nLIST OF FILE ATTACHMENTS PUBLIC URLs:\n\n`;
 export const reasoning_tag = "<think>";
 export const reasoning_end_tag = "</think>";
