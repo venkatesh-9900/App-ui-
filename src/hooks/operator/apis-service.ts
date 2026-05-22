35994a3230bd9678cfc12690eaa7b0a5441570e3
pknxvii <preetamnahak.nitr@gmail.com>
2026-05-22 14:08:28 +0530

    add ui  to get permission unbounded apis in mappings page

--- src/hooks/operator/apis-service.ts
+++ src/hooks/operator/apis-service.ts
@@ -59,6 +59,43 @@ export const fetchApis = async ({ successTask, failureTask, errorTask, forbidden
     }
 };
 
+export const fetchUnboundApis = async ({ successTask, failureTask, errorTask, forbiddenTask, page, limit, search, retry = false }: BaseServiceParams) => {
+    try {
+        if (retry) {
+            await refreshAccessToken({ failureTask, errorTask });
+        }
+
+        const queryParams = new URLSearchParams();
+        if (page !== undefined) queryParams.append("page", (page - 1).toString());
+        if (limit !== undefined) queryParams.append("limit", limit.toString());
+        if (search) queryParams.append("search", search);
+
+        const url = queryParams.toString()
+            ? `${ENDPOINTS.OPERATOR.UNBOUND_APIS}?${queryParams.toString()}`
+            : ENDPOINTS.OPERATOR.UNBOUND_APIS;
+
+        const headers = { ...buildHeaderJSON(false), "x-app-name": app_name };
+        const response = await fetch(url, { method: 'GET', headers });
+
+        if (response.status === 401) {
+            if (retry) {
+                await reauthenticationStep(errorTask);
+            } else {
+                await fetchUnboundApis({ retry: true, successTask, failureTask, errorTask, forbiddenTask, page, limit, search });
+            }
+        } else if (response.status === 403) {
+            forbiddenTask?.();
+        } else if (response.ok) {
+            const data = await response.json();
+            successTask(data);
+        } else {
+            failureTask();
+        }
+    } catch (error) {
+        errorTask();
+    }
+};
+
 export const createApi = async ({ request, successTask, failureTask, errorTask, forbiddenTask, retry = false }: CreateApiParams) => {
     try {
         if (retry) {
